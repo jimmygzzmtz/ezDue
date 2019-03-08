64af8b8e9928f4a9983fca262fa2e658a6eb6423
Jaime González <jimmygzzmtz@gmail.com>
2019-03-08 00:42:20 -0600

    Tasks
    Added a task function that uses RegEx to save taks by title and date, including hour. Task tab now shows in tabs, like Finances.

--- src/app/productivity-modal/productivity-modal.page.ts
+++ src/app/productivity-modal/productivity-modal.page.ts
@@ -9,6 +9,15 @@ import { Storage } from '@ionic/storage';
 })
 export class ProductivityModalPage implements OnInit {
 
+  taskString: any;
+
+  dateRegex: any = [/(for)?\s*(today|tomorrow|in a day|in \d+ days|next week|in a week|in \d+ weeks)/ig, 
+    /(for|on)?\s*(next)?\s*(monday|tuesday|wednesday|thursday|friday|saturday|sunday)/ig,
+    /(for|on)?\s*(january \d+|february \d+|march \d+|april \d+|may \d+|june \d+|july \d+|august \d+|september \d+|october \d+|november \d+|december \d+)/ig];
+
+  hourRegex: any = [/(at)?\s*((\d+)\s*(am|pm))/ig, 
+    /(at)?\s*((\d+):(\d+)\s*(am|pm))/ig, /(at)?\s*((\d+):(\d+))/ig, /at\s*(\d+)/ig];
+
   constructor(public modalController: ModalController, public alertController: AlertController, private storage: Storage ) {
     
    
@@ -19,6 +28,182 @@ export class ProductivityModalPage implements OnInit {
 
   dismiss(){
     this.modalController.dismiss();
+  }
+
+  async save(){
+
+    var title = this.taskString;
+    var date;
+    var hour;
+
+    for (let i = 0; i < this.dateRegex.length; i++){
+      let result = this.taskString.match(this.dateRegex[i]);
+      title = title.replace(result, '').trim();
+      if (date == undefined && result != null){
+        date = result[0];
+      }   
+    }
+
+    for (let i = 0; i < this.hourRegex.length; i++){
+      let result = this.taskString.match(this.hourRegex[i]);
+      title = title.replace(result, '').trim();
+      if (hour == undefined && result != null){
+        hour = result[0];
+      }
+    }
+
+    //default value for date
+    if(date != undefined){
+      date = this.stringToDate(date);
+    }
+    if(date == undefined){
+      date = new Date;
+    }
+  
+    date = this.stringToTime(hour, date);
+
+    var task = {
+      title: title,
+      date: date,
+    }
+    
+    this.modalController.dismiss(task);
+    
+  }
+
+
+  //convert date string to real date
+  stringToDate(date){
+    var newDate = new Date;
+    var result = new Date(newDate.valueOf());
+
+    if(date.match(/today/i)){
+      return newDate;
+    }
+
+    if(date.match(/tomorrow|in a day/i)){
+      result.setDate(result.getDate() + 1);
+      newDate = result;
+      return newDate;
+    }
+
+    if(date.match(/in \d+ days/i)){
+      result.setDate(result.getDate() + +date.match(/\d+/));
+      newDate = result;
+      return newDate;
+    }
+
+    if(date.match(/next week|in a week/i)){
+      result.setDate(result.getDate() + 7);
+      newDate = result;
+      return newDate;
+    }
+
+    if(date.match(/in \d+ weeks/i)){
+      result.setDate(result.getDate() + (+date.match(/\d+/)*7));
+      newDate = result;
+      return newDate;
+    }
+
+    if(date.match(/monday/i)){
+      return dayMatch(1);
+    }
+
+    if(date.match(/tuesday/i)){
+      return dayMatch(2);
+    }
+
+    if(date.match(/wednesday/i)){
+      return dayMatch(3);
+    }
+
+    if(date.match(/thursday/i)){
+      return dayMatch(4);
+    }
+
+    if(date.match(/friday/i)){
+      return dayMatch(5);
+    }
+
+    if(date.match(/saturday/i)){
+      return dayMatch(6);
+    }
+
+    if(date.match(/sunday/i)){
+      return dayMatch(7);
+    }
+
+    if(date.match(/january \d+/i)){
+      return monthMatch(0);
+    }
+    if(date.match(/february \d+/i)){
+      return monthMatch(1);
+    }
+    if(date.match(/march \d+/i)){
+      return monthMatch(2);
+    }
+    if(date.match(/april \d+/i)){
+      return monthMatch(3);
+    }
+    if(date.match(/may \d+/i)){
+      return monthMatch(4);
+    }
+    if(date.match(/june \d+/i)){
+      return monthMatch(5);
+    }
+    if(date.match(/july \d+/i)){
+      return monthMatch(6);
+    }
+    if(date.match(/august \d+/i)){
+      return monthMatch(7);
+    }
+    if(date.match(/september \d+/i)){
+      return monthMatch(8);
+    }
+    if(date.match(/october \d+/i)){
+      return monthMatch(9);
+    }
+    if(date.match(/november \d+/i)){
+      return monthMatch(10);
+    }
+    if(date.match(/december \d+/i)){
+      return monthMatch(11);
+    }
+
+    function monthMatch(index) {
+      if((+newDate.getMonth() == index && +newDate.getDate() > +date.match(/\d+/)) || +newDate.getMonth() > index){
+        newDate.setFullYear(+newDate.getFullYear() + 1);
+      }  
+      newDate.setMonth(index);
+      newDate.setDate(+date.match(/\d+/));
+      return newDate;
+    } 
+
+    function dayMatch(index) {
+      newDate.setDate(newDate.getDate() + (index+(7-newDate.getDay())) % 7);
+      return newDate;
+    } 
+
+  }
+
+  stringToTime(hour, date){
+    var hourNum = +(hour.match(/\d+/))
+
+    if(hour.match(/pm/)){
+      hourNum = hourNum + 12;
+    }
+
+    let newDate = new Date;
+
+    if(+newDate.getHours() > hourNum && newDate.getDate() == date.getDate() && newDate.getMonth() == date.getMonth() && newDate.getFullYear() == date.getFullYear()){
+      var result = new Date(date.valueOf());
+      result.setDate(result.getDate() + 1);
+      date = result;
+    }
+
+    date.setHours(hourNum,0,0,0);
+
+    return date;
   }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,9 +10,44 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  tasks: any = [];
 
   constructor(public navCtrl: NavController, public modalController: ModalController, private storage: Storage){
     
+    this.storage.get('tasksArr').then((val) => {
+      if (val != "[]"){
+       this.tasks = JSON.parse(val)
+      }
+      else{
+       this.storage.set('tasksArr', JSON.stringify(this.tasks));
+      }
+    });
+
+  }
+
+  getCards() {
+    if (this.tasks != undefined){
+ 
+     var tasks2 = this.tasks;
+     tasks2.sort((a: any, b: any) => {
+        var aDate = new Date(a.date.valueOf());
+        var bDate = new Date(b.date.valueOf());
+        return aDate.getTime() - bDate.getTime();
+      }); 
+      return tasks2;
+ 
+      } 
+  }
+
+  removeCard(card) {
+
+    let index = this.tasks.indexOf(card);
+
+        if(index > -1){
+            this.tasks.splice(index, 1);
+            this.storage.set('tasksArr', JSON.stringify(this.tasks));
+        }
+
   }
 
   async logProductivity() {
@@ -24,7 +59,19 @@ export class Tab2Page {
 
     modal.onDidDismiss()
       .then((data) => {
-          
+          if (data.data != undefined){
+            if (this.tasks == null){
+              this.tasks = [];
+              this.tasks.push({
+                title: data.data.title,
+                date: data.data.date
+              })
+            }
+            else{
+              this.tasks.push(data.data);
+            }
+            this.storage.set('tasksArr', JSON.stringify(this.tasks));
+          }
     });
 
     await modal.present(); 
