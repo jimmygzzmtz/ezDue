7bf3c187eaf0f21283de60d57b199d52163ccebb
Jaime González <jimmygzzmtz@gmail.com>
2020-02-11 20:59:17 +0000

    Ionic 5 and Angular 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'finance-modal', loadChildren: './finance-modal/finance-modal.module#FinanceModalPageModule' },
-  { path: 'productivity-modal', loadChildren: './productivity-modal/productivity-modal.module#ProductivityModalPageModule' },
-  { path: 'balance-modal', loadChildren: './balance-modal/balance-modal.module#BalanceModalPageModule' }
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) },
+  { path: 'finance-modal', loadChildren: () => import('./finance-modal/finance-modal.module').then(m => m.FinanceModalPageModule) },
+  { path: 'productivity-modal', loadChildren: () => import('./productivity-modal/productivity-modal.module').then(m => m.ProductivityModalPageModule) },
+  { path: 'balance-modal', loadChildren: () => import('./balance-modal/balance-modal.module').then(m => m.BalanceModalPageModule) }
 ];
 @NgModule({
   imports: [
